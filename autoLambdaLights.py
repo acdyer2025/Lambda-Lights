cd20c67efdd99c5bc66b5eb3a4cf5f221ab3a38b
Adam <adamcdyer1@gmail.com>
2022-08-14 23:30:18 -0500

    Refactored for better control flow

--- autoLambdaLights.py
+++ autoLambdaLights.py
@@ -9,7 +9,7 @@ import json
 
 headers = requests.utils.default_headers()
 
-f = open("config.json", "r")
+f = open(".\\config.json", "r")
 config = json.load(f)
 f.close()
 
@@ -17,74 +17,78 @@ deviceList = list(config['deviceAddresses'])
 
 def main():
 
-    #test if built in mobile hotspot is turned on, if not turn it on
-    
-    try:
-        if(ping(config['hotspotAddress'], 1) == False):
-            print("Starting Hotspot...")
-            subprocess.Popen([r'.\batchFiles\hotspot.bat'])
-        else:
-            print("Hotspot Connected")
-    except:
-        print("Hotspot could not be started, or is already turned on. Please check batchFiles\hotspot.bat")
-    
-    pingCheck = 0
-    connectedDevices = 0
-    while(pingCheck < config['initialConnectionTimeout'] and connectedDevices != len(config['deviceAddresses'])):
-        for i in range(len(config['deviceAddresses'])):    
-            if(ping(config['deviceAddresses'][deviceList[i]], 1) == False):
-                print(deviceList[i] + " is not connected.\n")
-            else:
-                print(deviceList[i] + "Successfully Connected")
-                connectedDevices += 1
-        pingCheck += 1
-            
-    if(connectedDevices == 0):
-        print('Error: All devices timed out on the initial connection attempt. Please make sure devices are turned on and IP addresses are set correctly')
-        print('Contiuing to ping for devices in background')
-        while(connectedDevices == 0):
-            if(ping(config['hotspotAddress'], 1) == False):
-                print("The hotspot is no longer connected")
-            else:
-                for i in range(len(config['deviceAddresses'])):    
-                    if(ping(config['deviceAddresses'][deviceList[i]], 1) == True):
-                        print(deviceList[i]+ "Successfully Connected. Verifying LedFx...")
-                        connectedDevices += 1
-
-    elif(connectedDevices == len(config['deviceAddresses'])):
-        print('All Devices connected. Verifying LedFx connection...')
-    else:
-        print('Warning: Some devices timed out on the initial connection attempt. Check IP Addresses for those particular devices.')
-        print('Verifying LedFx connection...')
-
-    #Check to make sure that LedFx started correctly
-    #Also restarts LedFx in the event of a crash (Somewhat common)
+    state = 0
+    connectedDevices = list()
+    for i in range(len(config['deviceAddresses'])):
+        connectedDevices.append(False)
 
     while(True):
-        if(ping(config['hotspotAddress'], 1) == False):
-            print("The hotspot is no longer connected")
-        else:
-            for i in range(len(config['deviceAddresses'])):
-                try:
-                    response = requests.get("http://" + config['deviceAddresses'][deviceList[i]] + "/json/info")
-                    responseObject = json.loads(response.content)
-                except Exception as e:
-                    print("unable to connect to " + deviceList[i] + ". Exception")
-                    print(e)
-                    print(deviceList[i] + " has disconnected from the network")
+        while(state == 0):#test if built in mobile hotspot is turned on, if not turn it on    
+            try:
+                if(ping(config['hotspotAddress'], 1) == False):
+                    print("Starting Hotspot...")
+                    subprocess.Popen([r'.\batchFiles\\hotspot.bat'])
                 else:
-                    if(responseObject["live"] == False):
-                        print(deviceList[i] + " has disconnected from LedFx. Restarting LedFx")
-                        subprocess.Popen([r'.\batchFiles\stopLedFx.bat'])
-                        subprocess.Popen([r'.\batchFiles\startLedFx.bat', config['ledFxPath']])
-                        time.sleep(10)
+                    print("Hotspot Connected")
+                    state = 1
+            except:
+                print("Hotspot could not be started, or is already turned on. Please check batchFiles\hotspot.bat")
+
+        while(state == 1): 
+            print(state)   
+            for i in range(len(config['deviceAddresses'])):   
+                try: 
+                    if(ping(config['deviceAddresses'][deviceList[i]], 1) == False):
+                        print(deviceList[i] + " is not connected to hotspot.\n")
+                        connectedDevices[i] = False
                     else:
-                        print("Connected to hotspot, devices, and LedFx")
+                        print(deviceList[i] + " is connected to hotspot")
+                        connectedDevices[i] = True
+                except Exception as e:
+                    print(e)
+            
+            for i in range(len(connectedDevices)):
+                if(connectedDevices[i] == True):
+                    try:
+                        url = 'http://' + config['deviceAddresses'][deviceList[i]] + '/json/info'
+                        response = requests.request('GET', url)
+                        responseObject = json.loads(response.content)
+                        if(response.status_code != 200):
+                            connectedDevices[i] = False
+                            print(deviceList[i]+ 'Disconnected')
+                    except Exception as e:
+                        print("unable to connect to " + deviceList[i] + ". Exception")
+                        print(e)
+                    else:
+                        if(responseObject["live"] == False):
+                            print('LedFx has disconneced. Restarting LedFx')
+                            try:
+                                subprocess.Popen([r'.\batchFiles\stopLedFx.bat'])
+                            except Exception as e:
+                                print(e)
+                            try:
+                                subprocess.Popen([r'.\batchFiles\startLedFx.bat', config['ledFxPath']])
+                            except Exception as e:
+                                print(e)
+                            else:
+                                time.sleep(10)
+                        else:
+                            print(deviceList[i]+' is connected to LedFx')
+                else:
+                    pass
+            try:
+                if(ping(config['hotspotAddress'], 1) == False):
+                    state = 0
+            except:
+                state = 0
 
 def ping(host, packets):
     command = [r'.\batchFiles\ping.bat ', str(packets), host]
-    return subprocess.call(command, stdout=subprocess.DEVNULL) == 0
-
+    try:    
+        return subprocess.call(command, stdout=subprocess.DEVNULL) == 0
+    except Exception as e:
+        print(e)
+        return False
 
 if __name__ == "__main__":
     main()
